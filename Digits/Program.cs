d578004d20aefd93a6b313d0be833f0671220629
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2019-09-16 14:54:30 +0100

    Digits

---
+++ Digits/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int firstDigit = n / 100;
+            int secondDigit = n % 100 / 10;
+            int thirdDigit = n % 100 % 10;
+            int rows = firstDigit + secondDigit;
+            int columns = firstDigit + thirdDigit;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (n % 5 == 0)
+                    {
+                        n -= firstDigit;
+                    }
+                    else if (n % 3 == 0)
+                    {
+                        n -= secondDigit;
+                    }
+                    else
+                    {
+                        n += thirdDigit;
+                    }
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
