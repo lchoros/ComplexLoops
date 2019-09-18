0f36cc09593a024c3cdfc63d0d09d2ab7705ac8c
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2019-09-18 12:06:16 +0100

    push

---
+++ DumbPasswords/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DumbPasswords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char l = char.Parse(Console.ReadLine());
+
+            //character one
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write("{0}{1} ", i, j);
+                    //for (int g = 0; g < length; g++)
+                    //{
+                    //    for (int g = 0; g < length; g++)
+                    //    {
+                    //        for (int g = 0; g < length; g++)
+                    //        {
+
+                    //        }
+                    //    }
+                    //}
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = 1;
+            int b = 1;
+            for (int i = 0; i < n-1; i++)
+            {
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+            Console.WriteLine(b);
+            //PrinSequence
+            //int n = int.Parse(Console.ReadLine());
+            //int a = 0;
+            //int b = 1;
+            //for (int i = 0; i < n; i++)
+            //{
+            //int next = a + b;
+            //a = b;
+            //b = next;
+            //Console.WriteLine(a);
+            //int next = a + b;
+            //a = b;
+            //b = next;
+            //}
+        }
+    }
+}
